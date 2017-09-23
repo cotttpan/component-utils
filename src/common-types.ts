3b7817aeb01456a60c8ea4552191ddf2eddc23d6
cotttpan <cotttpan@gmail.com>
2017-09-23 20:17:52 +0900

    feat(*): add commn-types.ts

---
+++ src/common-types.ts
@@ -0,0 +1,20 @@
+import { ComponentConstructor, FunctionalComponent } from 'preact';
+import { Diff } from '@cotto/utils.ts';
+
+export type ClassComponent<P, S> = ComponentConstructor<P, S> & { displayName?: string };
+
+export type AnyComponent<P = {}> = ClassComponent<P, any> | FunctionalComponent<P>;
+
+export interface ComponentEnhancer<InjectedProps, RequiredProps> {
+    <OwnProps extends InjectedProps>(
+        BaseComponent: AnyComponent<OwnProps>
+    ): ClassComponent<Diff<OwnProps, InjectedProps> & RequiredProps, any>;
+}
+
+export interface EventHandler {
+    (event: Event): void;
+}
+
+export type EventHandlers<T> = {
+    [P in keyof T]: EventHandler
+};
