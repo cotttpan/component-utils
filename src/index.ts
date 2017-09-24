a9a12531af647e737eba69fb40a0b34872bb298e
cotttpan <cotttpan@gmail.com>
2017-09-24 15:45:12 +0900

    chore(*): index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-/*
- * withHanders
- * withState
- * withContext
- * cache
- * CacheChild
- * shouldUpdate
- * pure
-
-*/
+export * from './common-types';
+export { withHandlers } from './withHandlers';
+export { withState } from './withState';
+export { withContext } from './withContext';
+export { mapProps } from './mapProps';
+export { shouldUpdate } from './shouldUpdate';
+export { pure } from './pure';
+export { ChildCacheContainer } from './ChildCacheContainer';
+export { getDisplayName } from './getDisplayName';
