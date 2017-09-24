8a1c36f513312754eccf7948c273a7daabdf8288
cotttpan <cotttpan@gmail.com>
2017-09-24 21:26:04 +0900

    chore(common-type): fix typs

--- src/common-types.ts
+++ src/common-types.ts
@@ -1,4 +1,4 @@
-import { ComponentConstructor, FunctionalComponent } from 'preact';
+import { ComponentProps, Component } from 'preact';
 import { Diff } from '@cotto/utils.ts';
 
 export type ClassComponent<P, S> = ComponentConstructor<P, S> & { displayName?: string };
@@ -6,10 +6,21 @@ export type ClassComponent<P, S> = ComponentConstructor<P, S> & { displayName?: 
 export type AnyComponent<P = {}> = ClassComponent<P, any> | FunctionalComponent<P>;
 
 export interface ComponentEnhancer<InjectedProps, RequiredProps> {
-    <OwnProps extends InjectedProps>(
-        BaseComponent: AnyComponent<OwnProps>
+    <OwnProps /* extends InjectedProps */>(
+        BaseComponent: AnyComponent<OwnProps & InjectedProps>
     ): ClassComponent<Diff<OwnProps, InjectedProps> & RequiredProps, any>;
 }
+
+export interface FunctionalComponent<PropsType> {
+    (props: PropsType & ComponentProps<this>, context?: any): JSX.Element;
+    displayName?: string;
+    defaultProps?: any;
+}
+
+export interface ComponentConstructor<PropsType, StateType> {
+    new(props: PropsType, context?: any): Component<PropsType, StateType>;
+}
+
 
 export interface EventHandler {
     (event: Event): void;
