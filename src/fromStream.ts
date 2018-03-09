e60fdcb6e274221fde38446acdc35ddd51e99ecc
cotttpan <cotttpan@gmail.com>
2018-03-09 22:58:18 +0900

    feat: add fromStream.ts

---
+++ src/fromStream.ts
@@ -0,0 +1,42 @@
+import { Component, ComponentConstructor } from 'preact'
+import { Observable } from 'rxjs/Observable'
+import { map } from 'rxjs/operators/map'
+import { Subscription } from 'rxjs/Subscription'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
+import { Renderer, renderProps } from './renderProps'
+
+export type StreamFactory<T, U> = (props: T, context: any)
+  => (props$: Observable<T>) => Observable<U>
+
+export function fromStream<T, U>(
+  name: string,
+  factory: StreamFactory<T, U>,
+): ComponentConstructor<T & Renderer<U>, any> {
+  return class FromStreamContainer extends Component<T & Renderer<U>, any> {
+    static displayName = name
+
+    state = { vdom: null }
+    props$ = new BehaviorSubject(this.props)
+    subscription: Subscription
+
+    constructor(props: any, context: any) {
+      super(props, context)
+      this.subscription = factory(props, context)(this.props$)
+        .pipe(map(state => renderProps(this.props, state)))
+        .subscribe(vdom => this.setState({ vdom }))
+    }
+    shouldComponentUpdate(_: any, nextState: any) {
+      return this.state.vdom !== nextState.vdom
+    }
+    componentWillReceiveProps(nextProps: T) {
+      this.props$.next(nextProps)
+    }
+    componentWillUnmount() {
+      this.props$.complete()
+      this.subscription.unsubscribe()
+    }
+    render() {
+      return this.state.vdom
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { shouldUpdate } from './shouldUpdate'
 export { pure } from './pure'
 export { getDisplayName } from './getDisplayName'
 export { If } from './If'
+export { fromStream, StreamFactory } from './fromStream'
 /* utils */
 export { renderProps, Renderer } from './renderProps'
 export { store } from './store'
